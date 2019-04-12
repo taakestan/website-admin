936d988f6bc373cd05b27029dd8948cb1c94d84c
HamidNE <hamidne@mail.ru>
2019-04-12 21:59:44 +0430

    add plugins to project

---
+++ plugins/axios.js
@@ -0,0 +1,12 @@
+export default function ({ $axios, redirect, app, store }) {
+  $axios.onResponse(response => {
+    if (response.status === 201) store.dispatch('errors/clear');
+    if (response.status === 204) app.$toast.success('حذف با موفقیت انجام شد.');
+    if (app.$auth.loggedIn && response.data.message) app.$toast.success(response.data.message);
+  });
+
+  $axios.onError(error => {
+    app.$toast.error(error.response.data.message);
+    if (error.response.status === 422) store.dispatch('errors/record', error.response.data.errors)
+  });
+}
---
+++ store/index.js
@@ -0,0 +1,4 @@
+export const actions = {
+  async nuxtClientInit({ dispatch }) {
+  },
+};
