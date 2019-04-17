8d67dc9227f8a0fb6e1791f8ddb437d0f6a0306a
HamidNE <hamidne@mail.ru>
2019-04-17 19:19:22 +0430

    add webinars store file

---
+++ store/webinars.js
@@ -0,0 +1,19 @@
+const collectionName = 'webinars';
+
+export const state = () => ({all: []});
+
+export const mutations = {
+  setItems(state, item) {
+    state.all = item;
+  },
+};
+
+export const actions = {
+  async prepare({commit}) {
+    const data = [];
+    const response = await this.$fireStore.collection(collectionName)
+        .orderBy("holding_at", "desc").get();
+    await response.forEach(doc => data.push({slug: doc.id, ...doc.data()}));
+    commit('setItems', data);
+  },
+};
