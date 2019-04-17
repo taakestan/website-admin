5de9c3a377f6a15bbc6f54823d8d9ffe9aad9bc2
HamidNE <hamidne@mail.ru>
2019-04-17 18:13:40 +0430

    add font style to project

--- layouts/default.vue
+++ layouts/default.vue
@@ -63,6 +63,7 @@
           { rel: 'stylesheet', href: '/css/menu-light.rtl.css' },
           { rel: 'stylesheet', href: '/css/brand-navy.rtl.css' },
           { rel: 'stylesheet', href: '/css/aside-navy.rtl.css' },
+          { rel: 'stylesheet', href: 'https://cdn.rawgit.com/rastikerdar/shabnam-font/v4.0.0/dist/font-face.css' },
         ]
       }
     }
