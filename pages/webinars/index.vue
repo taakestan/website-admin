c76269bdc476d08b369ece2d29d51f92c7756b22
HamidNE <hamidne@mail.ru>
2019-04-17 19:13:25 +0430

    edit for webinars page layouts and styles

--- pages/webinars/index.vue
+++ pages/webinars/index.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="container mt-3">
-    <div class="d-flex justify-content-between">
-      <h3>لیست وبینار ها</h3>
+  <div>
+    <div class="kt-subheader kt-grid__item" id="kt_subheader">
+      <div class="kt-subheader__main">
+        <h3 class="kt-subheader__title">لیست وبینار ها</h3>
+
+        <span class="kt-subheader__separator kt-hidden"></span>
+        <div class="kt-subheader__breadcrumbs">
+          <a href="#" class="kt-subheader__breadcrumbs-home"><i class="flaticon2-shelter"></i></a>
+          <span class="kt-subheader__breadcrumbs-separator"></span>
+          <a href="" class="kt-subheader__breadcrumbs-link">
+            Extended </a>
+          <span class="kt-subheader__breadcrumbs-separator"></span>
+          <a href="" class="kt-subheader__breadcrumbs-link">
+            Icons </a>
+          <span class="kt-subheader__breadcrumbs-separator"></span>
+          <a href="" class="kt-subheader__breadcrumbs-link">
+            Flaticon </a>
+          <!-- <span class="kt-subheader__breadcrumbs-link kt-subheader__breadcrumbs-link--active">Active link</span> -->
+        </div>
+
+      </div>
       <nuxt-link
               class="btn btn-success"
               to="/admin/webinars/create">
+        <i class="la la-plus"></i>
         ایجاد وبینار
       </nuxt-link>
     </div>
-    <div class="row mt-3">
-      <div class="col-xl-4 col-lg-6 mb-3"
-           v-for="webinar in webinars" :key="webinar.id">
-        <webinar-card :webinar="webinar" :href="'/admin/webinars/' + webinar.slug"/>
+    <div class="kt-content  kt-grid__item kt-grid__item--fluid" id="kt_content">
+      <div class="row mt-3">
+        <div class="col-xl-4 col-lg-6 mb-3"
+             v-for="webinar in webinars" :key="webinar.id">
+          <webinar-card :webinar="webinar" :href="'/admin/webinars/' + webinar.slug"/>
+        </div>
       </div>
+      <!--    <pagination :meta="meta"/>-->
     </div>
-<!--    <pagination :meta="meta"/>-->
   </div>
 </template>
 
