2078dba082e51bafc66960db98f065a9c57ece7b
HamidNE <hamidne@mail.ru>
2019-04-17 17:39:17 +0430

    edit for default layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,35 @@
 <template>
-  <div>
-    <nuxt />
+  <div id="container" class="m-0">
+    <header class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <nuxt-link class="navbar-brand mr-0 mr-md-2" to="/admin">
+          <h4 class="m-0 p-1 px-2 border rounded">Taak</h4>
+        </nuxt-link>
+        <div class="navbar-nav-scroll">
+          <ul class="navbar-nav bd-navbar-nav flex-row">
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/admin/webinars">وبینار ها</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/admin/providers">ارائه دهندگان</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <nuxt class="py-3"/>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+  export default {
+    middleware: 'auth'
+  }
+</script>
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+<style scoped>
+  #container {
+    min-height: 100vh;
+    background: #f8fafc;
+  }
 </style>
