49f7900e7475a8738c6c257d426105eb5ddd7451
HamidNE <hamidne@mail.ru>
2019-05-02 19:33:27 +0430

    edit for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,5 @@
 export default function ({store, redirect, route}) {
-  store.state.user != null && route.name === 'login' ? redirect('/') : '';
-  store.state.user == null && isAdminRoute(route) ? redirect('/login') : '';
-}
-
-function isAdminRoute(route) {
-  if (route.matched.some(record => record.path === '')) {
-    return true
-  }
+  store.state.user === null
+      ? redirect('/login')
+      : redirect('/');
 }
